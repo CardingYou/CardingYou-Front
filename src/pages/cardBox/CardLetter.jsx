6753183f642588a7e656740e00e1ebfbdfe5e931
EunJiJung <bianbbc87@gmail.com>
2024-09-22 13:16:58 +0900

    feat: 카드 뒤집기 애니메이션 추가
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- src/pages/cardBox/CardLetter.jsx
+++ src/pages/cardBox/CardLetter.jsx
@@ -4,7 +4,7 @@ import { onClickDownloadImage } from '../../utils/image/DownloadImg';
 
 export default function CardLetter() {
     const [date, setDate] = useState("yyyy.MM.dd");
-    const [open, setOpen] = useState(false);
+    const [flipped, setFlipped] = useState(false);
     const [content, setContent] = useState("사랑하는 엄마께, \n엄마, 안녕하세요?");
     const { play } = useTextToSpeech(content); 
 
@@ -14,16 +14,44 @@ export default function CardLetter() {
             <img src="assets/images/carding_for_you.png" alt="carding_for_you" />
             </div>
             <div className='mt-4 flex justify-center'>
-            <div className={`relative w-[60%] pb-[79%] bg-white rounded-xl overflow-hidden ${!open ? 'border-4 border-yellow' : ''}`}>
-                {
-                    open ? (
-                        <img className='absolute top-0 left-0 w-full h-full object-cover' src="assets/images/sample_letter_img.png" alt="카드 이미지" />
-                    ) : (
+                <div 
+                className={`relative w-[60%] pb-[79%] bg-white rounded-xl overflow-hidden ${flipped ? '' : 'border-4 border-yellow'}`}
+                onClick={() => setFlipped(!flipped)} 
+                style={{
+                    perspective: '1000px'
+                }}
+                >
+                    <div 
+                        className={`absolute w-full h-full transition-transform duration-700`} 
+                        style={{
+                        transformStyle: 'preserve-3d',
+                        transform: flipped ? 'rotateY(180deg)' : 'rotateY(0)',
+                        }}
+                    >
+                    {/* 카드 앞면 */}
+                    <div 
+                        className="absolute w-full h-full backface-hidden flex bg-white"
+                        style={{
+                            backfaceVisibility: 'hidden',
+                            transform: 'rotateY(0deg)',
+                        }}
+                    >
                         <div className='py-10 px-5 text-black text-left font-xs whitespace-pre-wrap'>
                             {content}
                         </div>
-                    )
-                }
+                    </div>
+
+                    {/* 카드 뒷면 */}
+                    <div 
+                    className="absolute w-full h-full backface-hidden flex justify-center items-center"
+                    style={{
+                        backfaceVisibility: 'hidden',
+                        transform: 'rotateY(180deg)',
+                    }}
+                    >
+                        <img className='absolute top-0 left-0 w-full h-full object-cover' src="assets/images/sample_letter_img.png" alt="카드 이미지" />
+                    </div>
+                </div>
             </div>
             </div>
             <div>
